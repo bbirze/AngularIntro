adcb7188dfbe21a5b567f7d32376c505b305470d
bbirze <bbirze@yahoo.com>
2019-02-05 14:45:53 -0600

    Remove tslab/out/* compiled files from repo

--- tslab/out/play.js
+++
@@ -1,9 +0,0 @@
-var age = 20;
-//var age: number = "Too Old!";
-console.log(age);
-function printPerson(name, age) {
-    console.log("Name: " + name + "  age: " + age);
-}
-printPerson('Billy', 8);
-//printPerson(8, 'Billy');
-//# sourceMappingURL=play.js.map
