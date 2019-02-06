c6d2a63451a973aae42e91caac3f85cd8ebc6f64
bbirze <bbirze@yahoo.com>
2019-02-06 07:42:28 -0600

    start in state that won't compile

--- tslab/DemoCats.ts
+++ tslab/DemoCats.ts
@@ -2,10 +2,10 @@ class Cat {
   name:string;
   type:string;
 
-  constructor(name:string, type:string) {
-    this.name = name
-    this.type = type
-  } 
+  // constructor(name:string, type:string) {
+  //   this.name = name
+  //   this.type = type
+  // } 
 
   meow() : void {
     console.log (`${this.name} says meow!`)
@@ -13,12 +13,12 @@ class Cat {
 }
 
 //does not compile even with no constructor
-// var cat1:Cat = { name:'fluffy', type:'tabby' }
-// var cat2:Cat = { name:'ginger', type:'simamese' }
-// var cat3:Cat = new Cat
-var cat1:Cat = new Cat('fluffy', 'tabby')
-var cat2:Cat = new Cat('ginger', 'simamese')
-var cat3:Cat = new Cat('', '')
+var cat1:Cat = { name:'fluffy', type:'tabby' }
+var cat2:Cat = { name:'ginger', type:'simamese' }
+var cat3:Cat = new Cat
+// var cat1:Cat = new Cat('fluffy', 'tabby')
+// var cat2:Cat = new Cat('ginger', 'simamese')
+// var cat3:Cat = new Cat('', '')
 
 
 var cats: Cat[] = [ cat1, cat2, cat3 ]
