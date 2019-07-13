c87c7254c52c51c27819f1c28a6338589e77148d
bbirze@yahoo.com <bbirze@yahoo.com>
2019-07-13 15:52:59 -0400

    Typescript fslide fixed, so fixed the code to demo it too

--- tslab/DemoCats.ts
+++ tslab/DemoCats.ts
@@ -1,25 +1,20 @@
 class Cat {
   name:string;
   type:string;
-
-  constructor(name:string, type:string) {
-    this.name = name
-    this.type = type
-  } 
-
-  meow() : void {
-    console.log (`${this.name} says meow!`)
-  }
 }
 
+
+function meow(cat:Cat) {
+  console.log (`${cat.name} says meow!`)
+}
 //does not compile even with no constructor
 // var cat1:Cat = { name:'fluffy', type:'tabby' }
 // var cat2:Cat = { name:'ginger', type:'simamese' }
 // var cat3:Cat = new Cat
-var cat1:Cat = new Cat('fluffy', 'tabby')
-var cat2:Cat = new Cat('ginger', 'simamese')
-var cat3:Cat = new Cat('', '')
+var cat1:Cat = { 'name':'fluffy', 'type':'tabby'}
+var cat2:Cat =  { 'name':'ginger', 'type':'simamese'}
+var cat3:Cat = new Cat()
 
 
 var cats: Cat[] = [ cat1, cat2, cat3 ]
-cats.forEach(cat => cat.meow())
+cats.forEach(cat => meow(cat))
